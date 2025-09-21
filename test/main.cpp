c62df7b899b92d953f25fe05be81b5e3a55f0b98
Adam4Ram3 <335800520@qq.com>
2025-09-21 14:26:12 +0800

    重构主程序，定义EchoServer类并实现连接和消息处理逻辑，移除不必要的日志输出

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,64 @@
 // file: /home/tbce/MyMuduo/test/main.cpp
-#include <iostream>
-#include "../Logger.h" // 假设 Logger.h 在源码根目录
+#include <string>
+#include <functional>
+#include <mymuduo/TcpServer.h>
+#include <mymuduo/Logger.h>
 
-int main() 
+class EchoServer
 {
-    // 设置日志级别，这样 INFO 级别的信息才能被看到
-    Logger::instance().setLogLevel(INFO);
+public:
+    EchoServer(EventLoop *loop,
+               const InetAddress &addr,
+               const std::string &name)
+        : server_(loop, addr, name),
+          loop_(loop)
+    {
+        // 注册回调函数
+        server_.setConnectionCallback(
+            std::bind(&EchoServer::onConnection, this, std::placeholders::_1));
 
-    LOG_INFO("Hello from my_test_server!");
-    LOG_ERROR("This is an error message.");
-    LOG_INFO("func=%s => fd=%d events=%d index=%d \n", __FUNCTION__, 1, 2, 1);
-    std::cout << "MyMuduo test server is running." << std::endl;
+        server_.setMessageCallback(
+            std::bind(&EchoServer::onMessage, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
 
+        // 设置合适的subloop线程数量
+        server_.setThreadNum(3);
+    }
+    void start()
+    {
+        server_.start();
+    }
+
+private:
+    // 连接建立或者断开的回调
+    void onConnection(const TcpConnectionPtr &conn)
+    {
+        if (conn->connected())
+        {
+            LOG_INFO("Connection UP : %s", conn->peerAddress().toIpPort().c_str());
+        }
+        else
+        {
+            LOG_INFO("Connection DOWN : %s", conn->peerAddress().toIpPort().c_str());
+        }
+    }
+
+    // 可读写事件回调
+    void onMessage(const TcpConnectionPtr &conn, Buffer *buf, Timestamp time)
+    {
+        std::string msg = buf->retrieveAllAsString();
+        conn->send(msg);
+        // conn->shutdown();   // 关闭写端 底层响应EPOLLHUP => 执行closeCallback_
+        (void) time;
+    }
+    TcpServer server_;
+    EventLoop *loop_;
+};
+
+int main()
+{
+    EventLoop loop;
+    InetAddress addr(8080);
+    EchoServer server(&loop, addr, "EchoServer");
+    server.start();
     return 0;
 }
